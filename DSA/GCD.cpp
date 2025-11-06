8437dad445f8c821a80c1da6df1f800aa535e9f4
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2025-11-06 20:08:37 +0530

    Update

---
+++ DSA/GCD.cpp
@@ -0,0 +1,58 @@
+//Find GCD of two numbers..
+/*#include<bits/stdc++.h>
+using namespace std;
+
+stack<int> findFact(int n){
+    stack<int> s;
+    
+    for(int i=1; i<=n/2; i++){
+        if(n % i == 0) s.push(i);
+    }
+    s.push(n);
+    
+    return s;
+}
+
+int main(){
+    int n1, n2;
+    cout<<"Enter first & second no..: ";
+    cin>>n1>>n2;
+    
+    stack<int> s1 = findFact(n1);
+    stack<int> s2 = findFact(n2);
+    
+    
+        while(s1.top() != s2.top()){
+            if(s1.top() > s2.top()) s1.pop();
+            else s2.pop();
+        }
+    
+    
+    cout<<"GCD is "<<s1.top();
+    
+    return 0;
+}*/
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b){
+    while(b != 0){
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int main(){
+    int n1, n2;
+    cout<<"Enter first & second no..: ";
+    cin >> n1 >> n2;
+    cout << "GCD is " << ((n1 > n2) ? gcd(n1, n2) : gcd(n2, n1));
+
+    return 0;
+}
+
