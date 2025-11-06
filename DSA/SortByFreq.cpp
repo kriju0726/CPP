b904ebeaf0889f3fb868beec0dbe8951c63b29ee
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2025-11-06 23:43:57 +0530

    Sort an Array by Frequency

---
+++ DSA/SortByFreq.cpp
@@ -0,0 +1,125 @@
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+// // Function to sort 2d array by frequency
+// void sort2darray(int array2d[2][100], int k)
+// {
+//   for (int i = 0; i < k - 1; i++)
+//   {
+//     for (int j = 0; j < k - 1 - i; j++)
+//     {
+//       if (array2d[1][j] < array2d[1][j + 1])
+//       {
+//         swap(array2d[1][j], array2d[1][j + 1]);
+//         swap(array2d[0][j], array2d[0][j + 1]);
+//       }
+//     }
+//   }
+// }
+// // Function to sort elements of array by frequency
+// void Sortele(int arr[], int n)
+// {
+//   // Step 1: Sort the array
+//   sort(arr, arr + n);
+//   // Step 2: Create a 2d array
+//   int arr2d[2][100];
+//   int k = 0;
+//   int count = 0;
+//   for (int i = 0; i < n; i++)
+//   {
+//     if (i == 0)
+//     {
+//       arr2d[0][k] = arr[i];
+//       count = 1;
+//     }
+//     else if (arr[i] == arr[i - 1])
+//     {
+//       count++;
+//     }
+//     else
+//     {
+//       arr2d[1][k] = count;
+//       count = 1;
+//       k++;
+//       arr2d[0][k] = arr[i];
+//     }
+//   }
+//   arr2d[1][k] = count;
+//   k++;
+ 
+//   // Step 3: sort the 2d array according to frequency
+//   sort2darray(arr2d, k);
+ 
+//   // Step 4: Store the answer in original array
+//   k = 0;
+//   int i = 0;
+//   while (i < n)
+//   {
+//     while (arr2d[1][k] > 0)
+//     {
+//       arr[i] = arr2d[0][k];
+//       i++;
+//       arr2d[1][k]--;
+//     }
+//     k++;
+//   }
+// }
+
+
+// int main()
+// {
+//   int n = 8;
+//   int arr[] = {1, 2, 3, 2, 4, 3, 1, 2};
+   
+//   Sortele(arr, n);
+  
+//   for (int i = 0; i < n; i++)
+//   {
+//     cout << arr[i] << " ";
+//   }
+  
+//   return 0;
+// }
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+bool sortele(pair<int, int> a, pair<int, int> b)
+{
+  if (a.second == b.second)
+    return a.first < b.first;
+  return a.second > b.second;
+}
+void Sortelementsbyfreq(int arr[], int n)
+{
+  unordered_map<int, int> map;
+  for (int i = 0; i < n; i++)
+  {
+    map[arr[i]]++;
+  }
+  vector<pair<int, int>> vec;
+  for (auto it = map.begin(); it != map.end(); it++)
+  {
+    vec.push_back({it->first, it->second});
+  }
+  sort(vec.begin(), vec.end(), sortele);
+  for (int i = 0; i < vec.size(); i++)
+  {
+    while (vec[i].second > 0)
+    {
+      cout << vec[i].first << " ";
+      vec[i].second--;
+    }
+  }
+  cout << endl;
+}
+int main()
+{
+  int arr[] = {3, 3, 5, 2, 1, 1, 2};
+  int n = 7;
+  Sortelementsbyfreq(arr, n);
+  return 0;
+}
