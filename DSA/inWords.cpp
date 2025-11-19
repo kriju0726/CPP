723f37782a090d5dcc56d248492c200aeee5be1f
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2025-11-19 23:39:05 +0530

    Roman To Integer

--- DSA/inWords.cpp
+++ DSA/inWords.cpp
@@ -7,24 +7,21 @@ void convertnumintoword(string str)
   string twodigits[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "ninteen", "twelve"};
   string tensmultiple[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninty"};
   string tenspower[] = {"hundred", "thousand"};
-  if (str.length() == 0)
-  {
+
+  if (str.length() == 0){
     cout << "" << endl;
     return;
   }
-  else if (str.length() == 1)
-  {
+  else if (str.length() == 1){
     cout << singledigit[str[0] - '0'] << endl;
     return;
   }
 
   int len = str.length();
-  for (int i = 0; i < str.length(); i++)
-  {
-    if (len > 2)
-    {
-      if (str[i] - '0' != 0)
-      {
+  
+  for (int i = 0; i < str.length(); i++){
+    if (len > 2){
+      if (str[i] - '0' != 0){
         cout << singledigit[str[i] - '0'] << " ";
         cout << tenspower[len - 3] << " ";
       }
@@ -32,13 +29,11 @@ void convertnumintoword(string str)
     }
     else      //(len <= 2 )
     {
-      if (str[i] - '0' == 1)
-      {
+      if (str[i] - '0' == 1){
         cout << twodigits[str[i + 1] - '0'] << " ";
         return;
       }
-      else if (str[i] - '0' != 0)
-      {
+      else if (str[i] - '0' != 0){
         cout << tensmultiple[str[i] - '0'] << " ";
         if (str[i + 1] - '0' != 0)
           cout << singledigit[str[i + 1] - '0'] << " ";
